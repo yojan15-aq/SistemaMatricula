cb69283b0e8f8d8a0274119fb99a6a2a1c36ad60
porni <porni@paolo>
2026-09-22 22:08:27 -0500

    tee

--- SistemaMatricula/src/gui/FrmAlumno.java
+++ SistemaMatricula/src/gui/FrmAlumno.java
@@ -14,7 +14,7 @@ import javax.swing.JButton;
 
 public class FrmAlumno extends JFrame {
 //hola
-	//lol
+	//lol s
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JLabel lblcodigo;
